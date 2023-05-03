879304b25789177d5d5cb51fc7795aa71e5551a0
Karolina Drabik <kd417818@students.mimuw.edu.pl>
2023-05-03 21:01:58 +0200

    Add more tests

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-  echo "Usage: ./script.sh <test_dir>"
+if [ $# -ne 2 ]; then
+  echo "Usage: ./script.sh <test_dir> <k>"
 else
   FILES="${1}/input/*"
   for f in ${FILES}
   do
     b=$(basename -- "$f")
     echo $b
-    ./kcliques "$f" 12 temp.txt
+    ./kcliques "$f" "${2}" temp.txt
     diff temp.txt "${1}/output/$b"
   done
   rm temp.txt
